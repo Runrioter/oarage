d939252db27c39a262f54c530831f37ff89a47e4
Runrioter <runrioter@qq.com>
2018-12-22 01:24:38 +0800

    chore: Tweaks tslint config

--- packages/oarage-server/src/oarageServer.ts
+++ packages/oarage-server/src/oarageServer.ts
@@ -1,39 +1,47 @@
 // Copyright 2018 Runrioter
 
-import * as K8s from '@kubernetes/client-node';
-import Koa = require('koa');
-import Router = require('koa-router');
+import { ApiType, Core_v1Api, KubeConfig } from "@kubernetes/client-node";
+import { Server } from "http";
+import Koa = require("koa");
+import Router = require("koa-router");
 
 class OarageServer extends Koa {
 
+  private readonly client: ApiType;
   private readonly router: Router;
-  private readonly client: K8s.ApiType;
 
-  constructor() {
+  public constructor() {
     super();
     this.router = new Router();
     this.registerRoutes();
 
-    const kc = new K8s.KubeConfig();
-    kc.loadFromCluster();
-    this.client = kc.makeApiClient(K8s.Core_v1Api);
+    const kc = new KubeConfig();
+    kc.loadFromDefault();
+    this.client = kc.makeApiClient(Core_v1Api);
   }
 
-  public run(port: number) {
-    this.use(this.router.routes())
-        .use(this.router.allowedMethods())
-        .listen(port);
+  public run(port: number): Server {
+    return this
+              .use(this.router.routes())
+              .use(this.router.allowedMethods())
+              .listen(port);
   }
 
   private registerRoutes(): void {
-    this.router.get('ListAllNamespaces', '/api/v1/pods', async ctx => {
-      try {
-        const res = await (this.client as K8s.Core_v1Api).listPodForAllNamespaces();
-        ctx.body = res;
-      } catch(err) {
-        ctx.body = (err as Error).message;
+    this.router.get(
+      "ListAllNamespaces",
+      "/api/v1/pods",
+      async (ctx: Koa.Context) => {
+        try {
+          const {
+            body
+          } = await (this.client as Core_v1Api).listPodForAllNamespaces();
+          ctx.body = body;
+        } catch(err) {
+          ctx.body = (err as Error).message;
+        }
       }
-    });
+    );
   }
 
 }
