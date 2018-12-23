2ca19127d7f6d76c194b5efd037cb853cc3959be
Runrioter <runrioter@qq.com>
2018-12-23 23:58:22 +0800

    chore: Add @oarage/web

--- packages/oarage-server/src/oarageServer.ts
+++ packages/oarage-server/src/oarageServer.ts
@@ -4,13 +4,14 @@ import { ApiType, Core_v1Api, KubeConfig } from "@kubernetes/client-node";
 import { Server } from "http";
 import Koa = require("koa");
 import Router = require("koa-router");
+import Static = require("koa-static");
 
 class OarageServer extends Koa {
 
   private readonly client: ApiType;
   private readonly router: Router;
 
-  public constructor() {
+  public constructor(private readonly staticRootPath: string) {
     super();
     this.router = new Router();
     this.registerRoutes();
@@ -22,16 +23,18 @@ class OarageServer extends Koa {
 
   public run(port: number): Server {
     return this
+              .use(Static(this.staticRootPath))
               .use(this.router.routes())
               .use(this.router.allowedMethods())
               .listen(port);
   }
 
   private registerRoutes(): void {
-    this.router.get(
+    const k8sRouter = this.router.prefix("/__kubernetes__");
+    k8sRouter.get(
       "ListAllNamespaces",
       "/api/v1/pods",
-      async (ctx: Koa.Context) => {
+      async ctx => {
         try {
           const {
             body
